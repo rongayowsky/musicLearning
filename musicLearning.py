9c62fd95164f477c4eeb4e1d6462abc4fbc7a723
Ronald Gayowsky <ron.gayowsky@gmail.com>
2020-06-21 12:01:10 -0400

    Finalized the user interface for now
    'command' group is now required through the parser.
    'list' was changed to 'display' to avoid keyword misuse.
    'edit' was added because it's super easy to make a typo

--- musicLearning.py
+++ musicLearning.py
@@ -12,10 +12,11 @@ help_description =  """
                     """
 parser = argparse.ArgumentParser(description=help_description)
 ## Action flags, only one can be used at a time
-command = parser.add_mutually_exclusive_group()
+command = parser.add_mutually_exclusive_group(required=True)
 command.add_argument('--add', action='store_true', help='Mutually exclusive flag to add a new song')
+command.add_argument('--edit', action='store_true', help='Mutually exclusive flag to edit an existing song.')
 command.add_argument('--remove', action='store_true', help='Mutually exclusive flag to remove an existing song')
-command.add_argument('--list', action='store_true', help='Mutually exclusive flag to list all stored songs')
+command.add_argument('--display', action='store_true', help='Mutually exclusive flag to display all stored songs')
 command.add_argument('--learn', action='store_true', help='Mutually exclusive flag to open stored media for a song')
 ## Specific arguments to detail songs, files, paths
 parser.add_argument('-s', '--song', type=str, required=False, nargs='?',
@@ -30,23 +31,38 @@ args = parser.parse_args()
 
 # Validate user input, select which function will be run.
 def main():
-    print("I don't do much right now.")
+    if args.display:
+        display_songs()
+    elif not args.song:
+        print('A song argument must be provided for this operation.')
+    elif args.add:
+        add()
+    elif args.edit:
+        edit()
+    elif args.remove:
+        remove()
+    elif args.learn:
+        display_material()
 
 # Add a song into the storage medium
 def add():
-    print("I don't do much right now.")
+    print("I will add a song to the storage medium!")
+
+# Edit a song from the storage medium
+def edit():
+    print("I will edit a song from the storage medium!")
 
 # Remove a song from the storage medium
 def remove():
-    print("I don't do much right now.")
+    print("I will remove a song from the storage medium!")
 
 # List all the songs stored in the storage medium
-def list():
-    print("I don't do much right now.")
+def display_songs():
+    print("I will display existing songs in the storage medium!")
 
 # Open the webpages and files stored for a song
 def display_material():
-    print("I don't do much right now.")
+    print("I will display the stored media so you can practice a song.")
 
 if __name__ == "__main__":
     main()
